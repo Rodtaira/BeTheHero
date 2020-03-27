f341c235c9b6842143c600376137f6c8e82955b9
rodtaira-movecode <rodrigotaira@movecode.com>
2020-03-27 17:06:33 -0300

    list specifics incidents method and session method

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,16 @@
-const express       =   require('express')
-const ongController =   require('./controllers/OngController')
-const incidentController = require('./controllers/IncidentController')
+const express               =   require('express')
+const ongController         =   require('./controllers/OngController')
+const incidentController    =   require('./controllers/IncidentController')
+const profileController     =   require('./controllers/ProfileController')
+const sessionController     =   require('./controllers/SessionController')
 const routes = express.Router()
 
 routes.get('/ongs', ongController.listOngs)
 routes.post('/ongs', ongController.createOng)
+
+routes.post('/session', sessionController.createSession)
+
+routes.get('/profile', profileController.listSpecificIncidents)
 
 routes.post('/incidents', incidentController.createIncident)
 routes.get('/incidents', incidentController.listIncidents)
